0e99a975617659e45810d7078e7b5e2bc10eb457
Mirco Kraenz <kraenzmirco@gmail.com>
2019-12-27 19:55:13 +0100

    ADD #1 helmet for improved security

--- src/configure-nest-app.ts
+++ src/configure-nest-app.ts
@@ -1,8 +1,10 @@
 import { INestApplication } from '@nestjs/common';
+import * as helmet from 'helmet';
 import { configuredLoggerMiddleware } from './common/middlewares/logger.middleware';
 import { configuredValidationPipe } from './common/pipes/validation.pipe';
 
 export const configureNestApp = (app: INestApplication) => {
+    app.use(helmet());
     app.useGlobalPipes(configuredValidationPipe);
     app.use(configuredLoggerMiddleware);
     app.enableCors();
